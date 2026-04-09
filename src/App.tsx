4f9e72a7adb430f9c832d09dcee197309eecf2b2
Yohan Viter <maiconviter23@hotmail.com>
2026-04-09 16:59:34 -0300

    aula 16

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,9 @@ function App() {
   console.log('Oi');
   return (
     <>
-      <Heading />
+      <Heading attr={123} attr2='String'>
+        Olá Mundo1!
+      </Heading>
       <p>
         Teste de yohan viter do curso de REACT para web do curso de chronos
         pomodoro.
