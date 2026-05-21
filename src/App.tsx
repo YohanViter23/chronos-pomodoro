f9683e5357a566504634e8f781ea33661c39a2c7
Yohan Viter <maiconviter23@hotmail.com>
2026-05-21 12:33:24 -0300

    Aula até 49

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,13 @@
 import { Home } from './pages/Home';
-import type { TaskStateModel } from './models/TaskStateModel';
+import { TaskContextProvider } from './contexts/TaskContext';
 
 import './styles/theme.css';
 import './styles/global.css';
-import { useState } from 'react';
-import { TaskContext } from './contexts/TaskContext';
-
-const initialState: TaskStateModel = {
-  tasks: [],
-  secondsRemaining: 0,
-  formattedSecondsRemaining: '00:00',
-  activeTask: null,
-  currentCycle: 0,
-  config: {
-    workTime: 25,
-    shortBreakTime: 5,
-    longBreakTime: 15,
-  },
-};
 
 export function App() {
-  const [state, setState] = useState(initialState);
-
-  console.log('APP', state);
-
   return (
-    <TaskContext.Provider value={{ outraCoisa: 321 }}>
+    <TaskContextProvider>
       <Home />
-    </TaskContext.Provider>
+    </TaskContextProvider>
   );
 }
