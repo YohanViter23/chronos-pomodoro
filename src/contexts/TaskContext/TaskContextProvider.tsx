7e3cd3d0082c4056ca301e8d66ce417a70f2067c
Yohan Viter <maiconviter23@hotmail.com>
2026-06-11 17:18:12 -0300

    Aula até 64

--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -1,6 +1,7 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useReducer, useState } from 'react';
 import { initialTaskState } from './initialTaskState';
 import { TaskContext } from './TaskContext';
+import { HandHelpingIcon } from 'lucide-react';
 
 type TaskContextProviderProps = {
   children: React.ReactNode;
@@ -9,13 +10,31 @@ type TaskContextProviderProps = {
 export function TaskContextProvider({ children }: TaskContextProviderProps) {
   const [state, setState] = useState(initialTaskState);
 
-  useEffect(() => {
-    console.log(state);
-  }, [state]);
+  const [numero, dispatch] = useReducer((state, action) => {
+    console.log(state, action);
+
+    switch (action) {
+      case 'INCREMENT':
+        return state + 1;
+      case 'DECREMENT':
+        return state - 1;
+      case 'INITIAL_STATE':
+        return 0;
+    }
+
+    return state;
+  }, 0);
+
+  //useEffect(() => {
+  //  console.log(state);
+  //}, [state]);
 
   return (
     <TaskContext.Provider value={{ state, setState }}>
-      {children}
+      <h1>o Número é: {numero}</h1>
+      <button onClick={() => dispatch('INCREMENT')}>Incrementar</button>
+      <button onClick={() => dispatch('DECREMENT')}>Decrementar</button>
+      <button onClick={() => dispatch('INITIAL_STATE')}>Zerar</button>
     </TaskContext.Provider>
   );
 }
